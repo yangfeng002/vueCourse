cb3d068769ef818bfc6c414994bc19f59b395410
yangfeng_ZR <yangfeng12345@139.com>
2020-08-31 16:46:29 +0800

    update

---
+++ component/child.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="app">
+          <input @click="sendMsg" type="button" value="给父组件传递值">
+       </div>
+
+</template>
+
+<script>
+   //模板里面有个固定的写法,向外默认暴露一个配置对象
+   export default {
+      name:'child',
+	  data(){
+		  return {
+			   msg: "我是子组件的msg"
+		  }
+	  },
+	  methods:{
+		   sendMsg(){
+			   //func: 是父组件指定的传数据绑定的函数，this.msg:子组件给父组件传递的数据
+			   this.$emit('func',this.msg)
+		   }
+	       }
+   }
+
+</script>
+
+<style>
+</style>
---
+++ component/parent.vue
@@ -0,0 +1,31 @@
+<template>
+   <div class="app">
+	   <child @fun ="getMsgFormSon" ></child>
+   </div>
+</template>
+
+<script>
+	import child from './child.vue';
+	//模板里面有个固定的写法,向外默认暴露一个配置对象
+	export default {
+	   name:'parent',
+		  data(){
+			  return {
+				 msgFormSon: "this is msg" 
+			  }
+		  },
+		  components:{
+			  child
+		  },
+		  methods:{
+			  getMsgFormSon(data){
+				  this.msgFormSon = data
+				  console.log(this.msgFormSon)
+			  }
+		  }
+	}
+	
+</script>
+
+<style>
+</style>
